ec4b72e356fcfe9cd04e426eccf78ade6a69b8c9
niloysikdar <niloysikdar30@gmail.com>
2021-05-20 16:25:51 +0530

    Created Joining Form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Switch } from "react-router-dom";
-import Join from "./components/Join";
+import Join from "./components/Join/Join";
 import Chat from "./components/Chat";
 
 const App = () => {
---
+++ client/src/components/Join/Join.jsx
@@ -0,0 +1,53 @@
+import { useRef } from "react";
+import { useHistory } from "react-router-dom";
+import "./Join.css";
+
+const Join = () => {
+  const history = useHistory();
+
+  const nameRef = useRef();
+  const roomRef = useRef();
+  var name, room;
+
+  const onJoinClicked = () => {
+    name = nameRef.current.value.trim();
+    room = roomRef.current.value.trim();
+    console.log(name, room);
+    if (name !== "" && room !== "") {
+      history.replace(`/chat?name=${name}&room=${room}`);
+    } else {
+      alert("Field cannot be empty");
+    }
+  };
+
+  return (
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join Room</h1>
+        <div>
+          <input
+            required
+            type="text"
+            placeholder="Your Name"
+            className="joinInput"
+            ref={nameRef}
+          />
+        </div>
+        <div>
+          <input
+            required
+            type="text"
+            placeholder="Room Name"
+            className="joinInput mt-20"
+            ref={roomRef}
+          />
+        </div>
+        <button className="button mt-20" type="submit" onClick={onJoinClicked}>
+          Join
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Join;
