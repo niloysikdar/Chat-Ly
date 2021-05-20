b751c1f4101995757e2f62107064e071530e49c4
niloysikdar <niloysikdar30@gmail.com>
2021-05-20 10:45:56 +0530

    Created Socket instance

--- server/index.js
+++ server/index.js
@@ -9,6 +9,14 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on("connection", (socket) => {
+  console.log("We have a new connection :)");
+
+  socket.on("disconnect", () => {
+    console.log("User have left :(");
+  });
+});
+
 app.use(router);
 
 server.listen(PORT, () => {
