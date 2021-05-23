ceaefc9feece4092655cea0eecb36365fb6e7ace
niloysikdar <niloysikdar30@gmail.com>
2021-05-23 15:53:01 +0530

    Changes

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(router);
-app.use(cors);
+app.use(cors());
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
